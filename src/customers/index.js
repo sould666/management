bdc6e4a5af85df23f0e3141c826bc64efb181d8a
kontakt59 <13307657+kontakt59@users.noreply.github.com>
2024-01-15 16:57:41 +0100

    Organizing structure of an app and creating customers files to handle customer screen

---
+++ src/customers/index.js
@@ -0,0 +1,4 @@
+export { default as CustomerScreen } from './CustomerScreen';
+export { default as CustomerList } from './CustomerList';
+export { default as CustomerCard } from './CustomerCard';
+export { default as AddCustomer } from './AddCustomer';
