f0dd625389b0b47e5381ce18eb3f5d21d106ed55
kontakt59 <13307657+kontakt59@users.noreply.github.com>
2024-01-15 16:27:08 +0100

    Documentation, modification of animation to heartbeat pulse

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Customer Pulse Management
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://menagement.semsos.eu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Main Page
         </a>
       </header>
     </div>
